442b2eba2317aea193abecf03f1f8384e69c5df1
SMARTSOFT\a.esakia <a.esakia@smartsoft.ge>
2024-05-28 22:54:05 +0400

    add vector functionality

--- AMath.Calculus/Vectors/Vector.cs
+++ AMath.Calculus/Vectors/Vector.cs
@@ -1,20 +1,27 @@
-﻿using System;
+﻿using AMath.Calculus.common.Points;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AMath.Calculus.Vectors
 {
-    public class Vector<T> where T : struct
+    public class Vector<T> where T : struct, IEquatable<T>
     {
-        public T XCoordinate {  get; set; }
-        public T YCoordinate { get; set; }
+        internal Point<T> StartPoint { get; set; }
+        internal Point<T> EndPoint { get; set; }
+        
+        public Vector(T startX, T startY, T endX, T endY) 
+        {  
+            StartPoint = new TwoDimensionalPoint<T>(startX, startY); 
+            EndPoint = new TwoDimensionalPoint<T>(endX, endY); 
+        }
 
-        public Vector(T x, T y)
+        public static Vector<T> operator + (Vector<T> first, Vector<T> second)
         {
-            XCoordinate = x;
-            YCoordinate = y;
+            return null;
         }
     }
 }
