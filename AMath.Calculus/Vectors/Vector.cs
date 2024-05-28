5cfb7fd82fa37f9330ba02f21df05b93548328ba
SMARTSOFT\a.esakia <a.esakia@smartsoft.ge>
2024-05-28 20:59:04 +0400

    add vector

---
+++ AMath.Calculus/Vectors/Vector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMath.Calculus.Vectors
+{
+    public class Vector<T> where T : struct
+    {
+        public T XCoordinate {  get; set; }
+        public T YCoordinate { get; set; }
+
+        public Vector(T x, T y)
+        {
+            XCoordinate = x;
+            YCoordinate = y;
+        }
+    }
+}
