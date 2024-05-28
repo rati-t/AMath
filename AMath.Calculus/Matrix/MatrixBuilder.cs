7306ddbd34749305c247f5903c51470c433a5539
Rati <72700343+rati-t@users.noreply.github.com>
2024-05-28 20:58:52 +0400

    genesis

---
+++ AMath.Calculus/Matrix/MatrixBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMath.Calculus.Matrix
+{
+    public abstract class MatrixBuilder<T>
+        where T : struct, IEquatable<T>
+    {
+        //public Matrix<T> Like(int rows, int columns)
+        //{
+        //    return new Matrix<T>(rows, columns);
+        //}
+
+        //public Matrix<T> Diagonal(int rows, int columns)
+        //{
+        //    return new DiagonalMatrix<T>(rows, columns);
+        //}
+    }
+}
