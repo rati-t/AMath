65a50ddbe19ac1073eba9fc6e50878b94d995e63
SMARTSOFT\a.esakia <a.esakia@smartsoft.ge>
2024-06-01 20:28:19 +0400

    ...

---
+++ AMath.Algorithms.Tests/AStarTests.cs
@@ -0,0 +1,24 @@
+﻿using AMath.Calculus.Matrices;
+using AMath.Calculus.Matrices.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMath.Algorithms.Tests
+{
+    public class AStarTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestStar()
+        {
+
+        }
+    }
+}
--- AMath.Algorithms/AStar.cs
+++ AMath.Algorithms/AStar.cs
@@ -1,7 +1,22 @@
 ﻿namespace Algorithm.PathFinding
 {
-    public class AStar
+    public static class AStar
     {
+        public struct Point
+        {
+            public int XCoordinate { get; set; }
+            public int YCoordinate { get; set; }
 
+            public Point(int x, int y)
+            {
+                XCoordinate = x;
+                YCoordinate = y;
+            }
+        }
+
+        public struct 
+
+
+        public static  FindShortestRoute()
     }
 }
